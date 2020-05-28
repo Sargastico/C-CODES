41d9a86920bafb3b58a46ed040a9cd3f64a91562
Sargastico <fernandocaro54@hotmail.com>
2020-05-28 17:52:45 -0300

    Add files via upload

---
+++ elasticsearchClient-golang.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	"github.com/elastic/go-elasticsearch"
+)
+
+type ZeebeEvent struct {
+	_id	interface{}
+}
+
+func main() {
+
+	// Create a context object for the API calls
+	ctx := context.Background()
+
+	// Declare an Elasticsearch configuration
+	cfg := elasticsearch.Config{
+		Addresses: []string{"http://localhost:9200"},
+		Username: "elastic",
+		Password: "changeme",
+	}
+
+	// Instantiate a new Elasticsearch client object instance
+	client, err := elasticsearch.NewClient(cfg)
+
+	// Exit the system if connection raises an error
+	if err != nil {
+		log.Fatalf("Elasticsearch connection error:", err)
+	}
+
+	var buf bytes.Buffer
+
+	//Read Json Query
+	query, err := ioutil.ReadFile("./query.json")
+	queryS := string(query)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	var b strings.Builder
+	b.WriteString(queryS)
+	read := strings.NewReader(b.String())
+
+
+	// Attempt to encode the JSON query and look for errors
+	err = json.NewEncoder(&buf).Encode(read)
+	if err != nil {
+		log.Println("Error encoding query: %s", err)
+
+	} else {
+		fmt.Println("json.NewEncoder encoded query:", read, "\n")
+
+		// Pass the JSON query to the Golang client's Search() method
+		res, err := client.Search(client.Search.WithContext(ctx),client.Search.WithBody(read))
+		if err != nil {
+			log.Println("Elasticsearch API ERROR:", err)
+
+		} else {
+
+			bodyBytes, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			bodyString := string(bodyBytes)
+			fmt.Println(bodyString)
+
+
+			zeebeparser := ZeebeEvent{}
+			json.Unmarshal([]byte(bodyString), &zeebeparser)
+
+			defer res.Body.Close()
+		}
+
+	}
+}
